b7ba1ce88d3deb11fc30eb5a607cb5f7acaf167e
onurbabur <47417351+onurbabur@users.noreply.github.com>
2021-10-29 21:25:45 +0300

    Create lapsunpassable.ps1
    LAPS PS Modüle yüklü olan makine üzerinde cmdlet çalıştırılmalıdır. 
    
    Ortamımızda LAPS tarafından parola atanmamış cihazların tespiti için ihtiyaç duyularak oluşturulmuştur.

---
+++ lapsunpassable.ps1
@@ -0,0 +1,7 @@
+$computers = (Get-ADComputer -SearchBase "OU=TURKEY-TR,OU=Accounts,DC=xxx,DC=xxx" -Properties * -filter *).name
+
+  foreach ($comp in $computers) {
+
+Get-AdmPwdPassword -ComputerName $comp | where {$_.password -eq $null} | select computername, Distinguishedname
+
+}
