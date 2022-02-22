299acb2752ced9cef0f079c493f0a39c43bd81c3
onurbabur <47417351+onurbabur@users.noreply.github.com>
2022-02-22 17:40:36 +0300

    Update ADPasswordUsage.ps1
    Belirtilen OU altında bulunan kullanıcıların Parola Değiştirilme tarihini, scriptin çalıştırıldığı gün olarak günceller.. 
    Bayram öncesi genellikle kullanma ihtiyacı duyulmaktadır :)

--- ADPasswordUsage.ps1
+++ ADPasswordUsage.ps1
@@ -1,13 +1,13 @@
 $users = (Get-ADUser -Properties * -filter * -SearchBase "OU=Accounts,DC=babur,DC=hol" | select samaccountname).SamAccountName
     foreach ($username in $users) {
         $user = Get-ADUser -identity $username -properties pwdlastset
-            #Before Changed
+            #Değişmeden önceki tarih
             [datetime]::FromFileTime($user.pwdlastset)
             $user.pwdlastset=0
             set-aduser -Instance $user
             $user.pwdlastset = -1
             set-aduser -Instance $user
             $user = Get-ADUser -identity $username -properties pwdlastset
-            #After Changed
+            #Değiştikten sonraki tarih
             [datetime]::FromFileTime($user.pwdlastset)
 }
