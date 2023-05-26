8d26f356a36259d3e29877813566acfc5c796b5a
onurbabur <47417351+onurbabur@users.noreply.github.com>
2023-05-27 02:01:09 +0300

    Delete lapsunpassable.ps1

--- lapsunpassable.ps1
+++
@@ -1,7 +0,0 @@
-$computers = (Get-ADComputer -SearchBase "OU=TURKEY-TR,OU=Accounts,DC=xxx,DC=xxx" -Properties * -filter *).name
-
-  foreach ($comp in $computers) {
-
-        Get-AdmPwdPassword -ComputerName $comp | where {$_.password -eq $null} | select computername, Distinguishedname
-
-}
