b9620bda8647ac8dce4ed656e112ede1cdb844e2
onurbabur <47417351+onurbabur@users.noreply.github.com>
2023-05-27 02:01:56 +0300

    Delete specialcharacterusage.ps1

--- specialcharacterusage.ps1
+++
@@ -1,7 +0,0 @@
-$objects = Get-ADUser -Filter * -SearchBase "OU=TURKEY-TR,OU=Accounts,DC=babur,DC=com" -Properties  samaccountname, pwdLastSet
-foreach ($object in $objects) {
-  if (($object.samaccountname -like '*ö*') -or ($object.samaccountname -like '*ı*') -or ($object.samaccountname -like '*ğ*')  ) {
-    Write-Host "Name contains '$'"
-    $object.samaccountname
-    } 
- } 
