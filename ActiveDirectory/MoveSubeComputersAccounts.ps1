84dba73aa09d38c20124589c3652c55c5e781544
onurbabur <47417351+onurbabur@users.noreply.github.com>
2023-06-06 16:19:08 +0300

    Add files via upload
    Computers OU su altında belirtilen prefix ve suffix değerlerine eşleşen computer hesaplarını yine belirtilen OU ya taşıma işlemini gerçekleştirir ve taşınan hesap bilgilerini belirtilen mail adreslerine iletme işlemini gerçekleştirir.

---
+++ ActiveDirectory/MoveSubeComputersAccounts.ps1
@@ -0,0 +1,73 @@
+﻿# Gerekli parametreleri ayarlayın
+$sourceOU = "CN=Computers,DC=babur,DC=com,DC=tr"
+$destinationOU = "OU=Store-Computer-Objects,OU=Stores-DP,OU=005-Stores,DC=babur,DC=com,DC=tr"
+$usernamePrefixes = @("PULSE", "VM")
+$suffix = "GPS"
+$senderEmailAddress = "infra@babur.com.tr"
+$smtpServer = "10.x.x.x"
+$smtpPort = 25
+
+# Active Directory modülünü içe aktar
+Import-Module ActiveDirectory
+$willmove = $null
+$willmove=@()
+
+# Taşınacak hesapları alın
+$accountsToMove = Get-ADComputer -SearchBase $sourceOU -Filter * | Where-Object {
+    $computerName = $_.Name
+    $prefixFound = $false
+    $suffixFound = $false
+
+
+    foreach ($prefix in $usernamePrefixes) {
+        if ($computerName -like "$prefix*") {
+             $willmove += $computerName
+            $prefixFound = $true
+            break
+           
+        }
+    }
+
+
+    if ($computerName -like "*$suffix") {
+        $willmove += $computerName
+        $suffixFound = $true
+    }
+
+    $suffixFound
+
+}
+
+# E-posta gönderme işlemi
+$mailBody = "Asagidaki hesaplar belirtilen OU'ya tasindi:`n`n"
+$mailBody += $willmove
+
+$mailParams = @{
+    SmtpServer = $smtpServer
+    Port = $smtpPort
+    UseSsl = $false
+    From = "infra@babur.com.tr"
+    To = "infra-alert@babur.com.tr"
+    Subject = "Hesap Tasima Bildirimi"
+    Body = $mailBody
+}
+
+# Hesapları belirtilen OU'ya taşıyın
+foreach ($account in $willmove) {
+    Get-ADComputer $account | Move-ADObject -TargetPath $destinationOU
+    
+}
+
+if ($willmove -ne $null)  {
+
+    write-host "tasinacak hesap var"
+    Send-MailMessage @mailParams
+
+}
+
+else {
+    
+    write-host "tasinacak hesap yok"
+
+}
+
