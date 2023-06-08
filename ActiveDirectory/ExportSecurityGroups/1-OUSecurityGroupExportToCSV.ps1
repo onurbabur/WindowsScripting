00d9b486f0f90014c0f3dc4a7335259b39efdb10
onurbabur <47417351+onurbabur@users.noreply.github.com>
2023-06-08 13:48:33 +0300

    Add files via upload
    1- Belirtilen OU da bulunan security groupları belirtilen alana .csv formatında export eder.
    2- Export edilen csv dosyalarını tek tek excele alarak her .csv için sheete kayıt eder. ve belirtilen alana exceli kayıt eder.

---
+++ ActiveDirectory/ExportSecurityGroups/1-OUSecurityGroupExportToCSV.ps1
@@ -0,0 +1,27 @@
+﻿# İçe aktarım modülü
+Import-Module ActiveDirectory
+
+# İlgili OU'daki güvenlik gruplarını al
+$ouDN = "OU=BI Users,DC=babur,DC=com,DC=tr"  # İlgili OU'nun DistinguishedName (DN) bilgisini girin
+$groups = Get-ADGroup -Filter * -SearchBase $ouDN
+
+# Her bir güvenlik grubu için CSV dosyasını oluşturma
+foreach ($group in $groups) {
+    $groupName = $group.Name
+    $groupMembers = Get-ADGroupMember -Identity $group.DistinguishedName -Recursive | Where-Object { $_.objectClass -eq "user" }
+    
+    # Grup üyelerini içeren bir özel nesne oluşturma
+    $membersObject = foreach ($member in $groupMembers) {
+        $user = Get-ADUser -Identity $member.DistinguishedName -Properties samaccountname, mail
+        [PSCustomObject]@{
+            SamAccountName = $user.samaccountname
+            Email = $user.mail
+        }
+    }
+    
+    # CSV dosyasını oluşturma ve verileri içeri aktarma
+    $csvPath = "C:\Users\adm-obabur\Desktop\BIReports\$groupName.csv"  # CSV dosyasının kaydedileceği konumu ve dosya adını belirtin
+    $membersObject | Export-Csv -Path $csvPath -NoTypeInformation
+}
+
+Write-Host "Tüm güvenlik grupları CSV dosyaları olarak dışa aktarıldı."
---
+++ ActiveDirectory/ExportSecurityGroups/2-CSVtoEXCEL.ps1
@@ -0,0 +1,55 @@
+﻿$csvFolderPath = "C:\Users\adm-obabur\Desktop\BIReports"
+$excelFilePath = "C:\Users\adm-obabur\Desktop\BIReports.xlsx"
+
+# Excel uygulamasını oluştur
+$excel = New-Object -ComObject Excel.Application
+$excel.Visible = $false
+$excel.DisplayAlerts = $false
+
+# Yeni bir Excel çalışma kitabı oluştur
+$workbook = $excel.Workbooks.Add()
+
+# CSV klasöründeki dosyaları al
+$csvFiles = Get-ChildItem -Path $csvFolderPath -Filter "*.csv"
+
+# Her CSV dosyası için
+foreach ($csvFile in $csvFiles) {
+    # Dosya adını al ve sheet adı olarak kullan
+    $sheetName = $csvFile.BaseName
+
+    # Yeni bir çalışma sayfası oluştur
+    $worksheet = $workbook.Worksheets.Add()
+    $worksheet.Name = $sheetName
+
+    # CSV dosyasını oku
+    $csvData = Import-Csv -Path $csvFile.FullName
+
+    # Verileri Excel sayfasına aktar
+    $rowIndex = 1
+    foreach ($row in $csvData) {
+        $columnIndex = 1
+        foreach ($property in $row.PsObject.Properties) {
+            $value = $property.Value
+            $worksheet.Cells.Item($rowIndex, $columnIndex) = $value
+            $columnIndex++
+        }
+        $rowIndex++
+    }
+
+    # İşlem tamamlandıktan sonra belleği temizle
+    [System.GC]::Collect()
+    [System.GC]::WaitForPendingFinalizers()
+}
+
+# Excel dosyasını kaydet ve kapat
+$workbook.SaveAs($excelFilePath)
+$excel.Quit()
+
+# Kullanılan kaynakları serbest bırak
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($worksheet) | Out-Null
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($workbook) | Out-Null
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel) | Out-Null
+[System.GC]::Collect()
+[System.GC]::WaitForPendingFinalizers()
+
+Write-Host "İşlem tamamlandı."
