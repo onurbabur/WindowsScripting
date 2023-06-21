5bf772f129ae2962c2cc25bf7a6e74818c2cee5b
onurbabur <47417351+onurbabur@users.noreply.github.com>
2023-06-21 17:35:12 +0300

    Add files via upload
    1-ExportLocalUser scripti calıstıgı bilgisayar üzerindeki local hesapları export edip, belirttiğimiz alana kayıt eder.
    2-CsvBirlestir scripti export edilen tüm csv leri tek bir excel altına birleştirir.

---
+++ ActiveDirectory/LocalUserReports/1-ExportLocalUser.ps1
@@ -0,0 +1,17 @@
+﻿$csvDosyasi = "\\dpprxxx01\OBTEST\Accounts\$env:COMPUTERNAME.csv"
+
+$makineAdi = $env:COMPUTERNAME
+$kullaniciListesi = Get-WmiObject -Class Win32_UserAccount -Filter "LocalAccount=True"
+
+$kullaniciSatirlari = @()
+
+foreach ($kullanici in $kullaniciListesi) {
+    $kullaniciAdi = $kullanici.Name
+    $durum = if ($kullanici.Disabled) { "Disable" } else { "Enable" }
+    $kullaniciSatir = "$makineAdi,$kullaniciAdi,$durum"
+    $kullaniciSatirlari += $kullaniciSatir
+}
+
+$kullaniciSatirlari | Out-File -FilePath $csvDosyasi -Encoding UTF8
+
+Write-Host "Kullanıcı listesi başarıyla kaydedildi: $csvDosyasi"
---
+++ ActiveDirectory/LocalUserReports/2-CsvBirlestir.ps1
@@ -0,0 +1,18 @@
+﻿$klasorYolu = "\\dpprxxx01\OBTEST\Accounts"
+$ciktiDosyasi = "\\dpprxxx01\OBTEST\Accounts\Tum_Kullanicilar.csv"
+
+# CSV dosyalarının tam yolunu al
+$csvDosyalari = Get-ChildItem -Path $klasorYolu -Filter "*.csv" | Select-Object -ExpandProperty FullName
+
+# Boş bir CSV dosyası oluştur
+$csvIcerik = @()
+a
+# Her CSV dosyasını oku ve içeriği birleştir
+foreach ($csvDosyasi in $csvDosyalari) {
+    $csvIcerik += Get-Content -Path $csvDosyasi
+}
+
+# Tüm içeriği çıktı dosyasına yaz
+$csvIcerik | Out-File -FilePath $ciktiDosyasi -Encoding UTF8
+
+Write-Host "Tüm kullanıcılar başarıyla birleştirildi: $ciktiDosyasi"
