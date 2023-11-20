b80e78e7add92e912082295bc80561b96a519e73
onurbabur <47417351+onurbabur@users.noreply.github.com>
2023-11-20 17:41:58 +0300

    Add files via upload

---
+++ Windows/TestUdpConnection.ps1
@@ -0,0 +1,19 @@
+﻿function Test-UDPConnection {
+    param (
+        [string]$ComputerName,
+        [int]$Port
+    )
+
+    $UDPClient = New-Object System.Net.Sockets.UdpClient
+    try {
+        $UDPClient.Connect($ComputerName, $Port)
+        "UDP connection to $ComputerName on port $Port succeeded."
+    } catch {
+        "UDP connection to $ComputerName on port $Port failed. Error: $_"
+    } finally {
+        $UDPClient.Close()
+    }
+}
+
+# Kullanım örneği:
+#Test-UDPConnection -ComputerName 192.168.1.1 -Port 1234
