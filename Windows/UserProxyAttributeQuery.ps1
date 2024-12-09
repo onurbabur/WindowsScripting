56eb4eb1a1d8249e4fd4824e4190e9c463bf5598
onurbabur <47417351+onurbabur@users.noreply.github.com>
2024-12-09 11:07:57 +0300

    Add files via upload

---
+++ Windows/UserProxyAttributeQuery.ps1
@@ -0,0 +1,8 @@
+﻿# İlgili kullanıcı adını kontrol etmek için PowerShell komutu
+$user = Get-ADUser -Filter { (UserPrincipalName -eq 'florya@xxx.com.tr') -or (ProxyAddresses -like 'florya@xxx.com.tr') }
+
+if ($user -ne $null) {
+    Write-Host "Kullanıcı bulundu: $($user.SamAccountName)"
+} else {
+    Write-Host "Kullanıcı bulunamadı."
+}
